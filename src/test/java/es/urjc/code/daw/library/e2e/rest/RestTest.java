335d49bd25599f2297a376eeb875c184d131ed90
ivannaGonzales <38204087+ivannaGonzales@users.noreply.github.com>
2021-07-05 18:59:31 +0200

    Update RestTest.java

--- src/test/java/es/urjc/code/daw/library/e2e/rest/RestTest.java
+++ src/test/java/es/urjc/code/daw/library/e2e/rest/RestTest.java
@@ -1,47 +1,47 @@
 package es.urjc.code.daw.library.e2e.rest;
-
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-
 import es.urjc.code.daw.library.book.Book;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DisplayName("REST tests")
 public class RestTest {
-
+ 
     @LocalServerPort
     int port;
-
+ 
+    private String host = System.getProperty("https://ais-ivanna2.herokuapp.com/", "http://localhost/");
+ 
     @BeforeEach
     public void setUp() {
+        if(host.equals("https://ais-ivanna2.herokuapp.com/")){
+            port = 8080;
+        }
         RestAssured.port = port;
+        RestAssured.baseURI = host;
     }
-
+ 
     @Autowired
     private ObjectMapper objectMapper;
-
+ 
     @Test
     @DisplayName("Añadir un nuevo libro y comprobar que se ha creado")
-	public void createBookTest() throws Exception {
-
+    public void createBookTest() throws Exception {
+ 
         // CREAMOS UN NUEVO LIBRO
-
-		Book book = new Book("FAKE BOOK","Contenido de prueba");
-    	
+ 
+        Book book = new Book("FAKE BOOK","Contenido de prueba");
         Book createdBook = 
-            given()
+             given()
                 .request()
                     .body(objectMapper.writeValueAsString(book))
                     .contentType(ContentType.JSON).
@@ -52,27 +52,24 @@ public class RestTest {
                 .statusCode(201)
                 .body("title", equalTo(book.getTitle()))
                 .extract().as(Book.class);
-
+ 
         // COMPROBAMOS QUE EL LIBRO SE HA CREADO CORRECTAMENTE
-
+ 
         when()
             .get("/api/books/{id}", createdBook.getId())
         .then()
              .assertThat()
              .statusCode(200)
              .body("title", equalTo(book.getTitle()));
-		
-    
     }
-
+ 
     @Test
-	@DisplayName("Borrar un libro y comprobar que se ha borrado")
-	public void deleteBookTest() throws Exception {
-
+    @DisplayName("Borrar un libro y comprobar que se ha borrado")
+    public void deleteBookTest() throws Exception {
+ 
         // CREAMOS UN NUEVO LIBRO
-
-		Book book = new Book("FAKE BOOK","Contenido de prueba");
-    	
+ 
+        Book book = new Book("FAKE BOOK","Contenido de prueba");
         Book createdBook = 
             given()
                 .request()
@@ -85,22 +82,20 @@ public class RestTest {
                 .statusCode(201)
                 .body("title", equalTo(book.getTitle()))
                 .extract().as(Book.class);
-        
         // BORRAMOS EL LIBRO CREADO
         when()
              .delete("/api/books/{id}",createdBook.getId())
         .then()
              .assertThat()
                 .statusCode(200);
-
+ 
         // COMPROBAMOS QUE EL LIBRO YA NO EXISTE
-
+ 
         when()
              .get("/api/books/{id}", createdBook.getId())
         .then()
              .assertThat()
                 .statusCode(404);
-
+ 
     }
-    
 }
